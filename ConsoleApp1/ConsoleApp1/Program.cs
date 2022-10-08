c163a2b01cae728fc1674002c3e43d6b4425e350
CeferovaSona <87385383+CeferovaSona@users.noreply.github.com>
2022-10-09 03:52:16 +0400

    add homeworks folder

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 5;
+            //int b = a;
+            //b = 18;
+            //Console.WriteLine(a + " " + b);
+
+
+            //int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //int[] numbers2 = numbers1;
+            //numbers2[0] = 99;
+
+            //Console.WriteLine(numbers1[0] + " " + numbers2[0]);
+
+            //int num = 13;
+            //num += 3;
+            //ShowNum(num);
+            //num -= 2;
+            //Console.WriteLine(num);
+
+
+
+            //int num;
+            //ShowNum(out num);
+            //num += 2;
+            //Console.WriteLine(num);
+
+            //int num;
+            //ShowNum(ref num);
+            //num += 2;
+            //Console.WriteLine(num);
+
+            //string name1;
+            //CheckStr(out name1);
+            //Console.WriteLine(name1);
+
+            //string str1 = "Cav";
+            //string str2 = "id";
+            //string name = str1 + str2;
+            //Console.WriteLine(name);
+
+
+            //StringBuilder str = new StringBuilder();
+            //str.Append("Cav");
+            //str.Append("id");
+
+            //Console.WriteLine(str);
+
+            //String name1 = new String("lale");
+            //Console.WriteLine(name1);
+
+            //int num = 55;
+
+            //Boolean isMarried = new Boolean();
+
+            //string name = "Hello Ramil";
+            //name = "";
+            //Console.WriteLine(name.Length);
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //Console.WriteLine(name.Trim());
+
+            //Console.WriteLine(name.StartsWith("R"));
+            //Console.WriteLine(name.EndsWith("l"));
+
+            //Console.WriteLine(name.ToLower());
+
+            //Console.WriteLine(name.ToUpper());
+
+            //if (name.StartsWith("R"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+            //if (name.Trim().ToLower().StartsWith("R"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+            //Console.WriteLine(name.IndexOf("a"));
+
+            //Console.WriteLine(name.LastIndexOf("a"));
+
+            //Console.WriteLine(name.Replace("Ramil","Resul"));
+
+            //Console.WriteLine(name.Remove(4,3));
+
+            //Console.WriteLine(name.Substring(2));
+
+            //var result = name.Split();
+
+
+            //var result = name.ToCharArray();
+
+            //Console.WriteLine(result);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //string[] word = { "one", "two", "three" };
+            //Console.WriteLine(string.Join(" ",word));
+
+            //string text = " ";
+
+            //if (string.IsNullOrEmpty(text))
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+            //if (string.IsNullOrWhiteSpace(text))
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+
+
+
+           
+            
+            //string a = "Hello";
+            //string b = "World";
+            //Console.WriteLine(string.Concat(a, b));
+
+            
+            //string name = "Mustafa";
+            //var result1 = name.Clone();
+            //name = "Ferid";
+            //Console.WriteLine(result1);
+
+           
+            //string name= "Mustafa";
+            //Console.WriteLine(name.Insert(0,"Hello, "));
+
+
+
+        }
+
+        //public static void CheckStr(out string str)
+        //{
+        //    str = "Mahir";
+        //    Console.WriteLine(str);
+        //}
+
+        //public static void ShowNum(ref int num)
+        //{
+        //    num += 2;
+        //    Console.WriteLine(num);
+        //}
+
+        //public static void ShowNum(out int num)
+        //{
+        //    num = 100;
+        //    num += 2;
+        //    Console.WriteLine(num);
+        //}
+    }
+}
